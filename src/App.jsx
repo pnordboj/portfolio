892c0a4523b8f191126e3f2e838e424b35b2d629
pnordboj <91888307+pnordboj@users.noreply.github.com>
2023-06-10 17:58:55 +0200

    Finished Portfolio Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,18 @@
-import React, { useState } from 'react';
-import { Header } from './components/Header/Header';
-import { Footer } from './components/Footer/Footer';
+import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import SharedLayout from './components/layout/SharedLayout';
 
 // Pages
 import Home from './pages/home/Home';
-import Projects from './pages/projects/Projects';
-import About from './pages/about/About';
 
 const App = () => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-
-  const toggleTheme = () => {
-    setIsDarkMode(!isDarkMode);
-  };
-
   return (
-    <div className={`h-screen ${isDarkMode ? 'dark' : 'light'}`}>
-      <Header isDarkMode={isDarkMode} toggleTheme={toggleTheme} />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/projects' element={<Projects />} />
-        <Route path='/about' element={<About />} />
-      </Routes>
-      <Footer isDarkMode={isDarkMode} />
+    <div>
+      <SharedLayout>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </SharedLayout>
     </div>
   );
 };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export const Header = () => {
   return (
     <header className='bg-gray-900'>
-      <nav className='flex items-center justify-between container mx-auto py-4'>
+      <nav className='flex items-center justify-between container mx-auto py-4 w-screen'>
         <Link to='/' className='flex items-center'>
           <h1 className='text-white text-xl font-bold'>Portfolio</h1>
         </Link>
